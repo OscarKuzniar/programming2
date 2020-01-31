1a3fd5bdb8f44996512bf52d34367ce70a1a7b75
Oscar Kuzniar <moskit1992@gmail.com>
2020-01-31 20:51:25 +0100

    filter method implemented

--- tdd/src/main/java/filter/NumbersFilter.java
+++ tdd/src/main/java/filter/NumbersFilter.java
@@ -1,14 +1,20 @@
 package filter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class NumbersFilter {
 
 
     public List<Integer> getOdd(List<Integer> numbers) {
-
-        return new ArrayList<>();
+        List<Integer> oddNumbers = new ArrayList<>();
+        for (int i = 0; i <numbers.size(); i++) {
+            if (numbers.get(i) % 2 != 0) {
+                oddNumbers.add(numbers.get(i));
+            }
+        }
+        return oddNumbers;
     }
 
     public List<Integer> getEven(List<Integer> numbers) {
