0be2acad53154298fac8cf0c00538efc7a43cf21
Oscar Kuzniar <moskit1992@gmail.com>
2020-01-31 20:42:43 +0100

    empty module added

---
+++ tdd/src/main/java/Run.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ tdd/src/test/java/TestSuite.java
@@ -0,0 +1,25 @@
+import org.junit.*;
+
+public class TestSuite {
+
+    @Before
+    public void before() {
+
+    }
+
+    @After
+    public void after() {
+
+    }
+
+    @AfterClass
+    public static void afterClass() {
+
+    }
+
+    @Test
+    public void testNothing() {
+
+    }
+
+}
